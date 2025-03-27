90211227c8f8ada9c6c2fc1fa3072399608a04de
saavedra75 <josemanuel75saavedra@gmail.com>
2025-03-27 17:36:22 +0100

    realizo ejercicio1

--- calentamiento.js
+++ calentamiento.js
@@ -1,1 +1,19 @@
-console.log("Ibai masón");
+/*Una variable numero cuyo valor sea -4.
+ Una variable palabra cuyo valor sea ”Hola Mundo”
+Una variable booleana cuyo nombre sea respuesta e inicialmente con valor true
+Crea una constante con nombre pi cuyo valor sea 3.14
+Modifica el valor de la variable numero para que en lugar de -4 sea 54 pero sin tocar la parte de la declaración de la variable.
+¿Qué pasa si modificas el valor de la variable antes de que esta sea declarada?
+Muestra por consola cada una de las variables. Usa console.log(nombre_variable); 
+Declara 2 variables y asigna los valores 5 y 2 respectivamente
+Muestra por consola la suma de ambas variables.
+Muestra por consola la resta de ambas variables.
+Muestra por consola la multiplicación de ambas variables.
+Muestra por consola la división de ambas variables.
+Muestra por consola la módulo de ambas variables.
+Declara una variable cuyo valor sea tu nombre.
+Muestra por consola el JAVASCRIPT texto Hola concatenado con el valor de la variable anterior.*/
+
+//1
+let numNeg = -4;
+
